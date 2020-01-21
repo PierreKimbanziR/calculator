7d6cb5826a6112769f0fc387dee35ac5f0d3686c
Pierre Kimbanzi Ruganda <kimbanzip@gmail.com>
2020-01-21 16:25:32 +0100

    add input display

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,7 @@ import "./Button.css";
 
 class Button extends Component {
   isOperator = val => {
-    return !isNaN(val) || val === "." || val === ",";
+    return !isNaN(val) || val === "." || val === "," || val === "=";
   };
   render() {
     return (
@@ -11,7 +11,7 @@ class Button extends Component {
         className={`button ${
           this.isOperator(this.props.children) ? "" : "operator"
         }`}
-        oncClick={() => this.props.handleClick(this.props.children)}
+        onClick={() => this.props.handleClick(this.props.children)}
       >
         {this.props.children}
       </div>
