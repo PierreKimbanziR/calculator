e655866ac574e5e93acb85a21881bba4a51e95f4
Pierre Kimbanzi Ruganda <kimbanzip@gmail.com>
2020-01-21 09:58:40 +0100

    add color to operators

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,8 +2,20 @@ import React, { Component } from "react";
 import "./Button.css";
 
 class Button extends Component {
+  isOperator = val => {
+    return !isNaN(val) || val === "." || val === ",";
+  };
   render() {
-    return <div className="button">{this.props.children}</div>;
+    return (
+      <div
+        className={`button ${
+          this.isOperator(this.props.children) ? "" : "operator"
+        }`}
+        oncClick={() => this.props.handleClick(this.props.children)}
+      >
+        {this.props.children}
+      </div>
+    );
   }
 }
 
